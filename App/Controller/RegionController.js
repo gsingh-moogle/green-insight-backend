1dc14ee29eea88a80fd429e26919b981afad7839
deepakk42 <gipscomm_deepak@os4w.de>
2023-01-01 10:09:27 +0530

    region controller api

--- App/Controller/RegionController.js
+++ App/Controller/RegionController.js
@@ -536,11 +536,11 @@ exports.getRegionEmissionData=async(req,res) => {
                     } 
                     count++;
                 }
-                // const data = {
-                //     contributor:contributor,
-                //     detractor:detractor
-                // };
-                const data = getRegionEmissions.map((item) => [item["Region.name"],item.contributor]);
+                const data = {
+                    contributor:contributor,
+                    detractor:detractor
+                };
+                //const data = getRegionEmissions.map((item) => [item["Region.name"],item.contributor]);
                 return Response.customSuccessResponseWithData(res,'Region Emissions',data,200)
             } else { return Response.errorRespose(res,'No Record Found!');}
     } catch (error) {
