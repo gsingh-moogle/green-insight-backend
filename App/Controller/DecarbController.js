3728cb22f6d474260270806e41138c47099ec01f
deepakk42 <85106163+deepakk42@users.noreply.github.com>
2023-02-14 18:25:07 +0530

    decarb changes

--- App/Controller/DecarbController.js
+++ App/Controller/DecarbController.js
@@ -7,6 +7,7 @@ const Emission =require("../models").Emission;
 const Decarb =require("../models").DecarbRecommendation;
 const Helper=require("../helper/common-helper");
 const moment = require('moment');
+const SQLToken = process.env.MY_SQL_TOKEN;
 
 exports.getRecommendedLevers=async(req,res) => {
     try {
@@ -27,12 +28,11 @@ exports.getRecommendedLevers=async(req,res) => {
             }
         }
         const Emissions = await Emission.findOne({
-            attributes: [[ sequelize.literal('( SELECT SUM(emission) )'),'emission'],[ sequelize.literal('( SELECT SUM(total_ton_miles) )'),'emission_per_ton']],
+            attributes: [[ sequelize.literal('( SELECT SUM(AES_DECRYPT(emission,"'+SQLToken+'")) )'),'emission'],[ sequelize.literal('( SELECT SUM(AES_DECRYPT(total_ton_miles,"'+SQLToken+'")) )'),'emission_per_ton']],
             where:where,
             raw:true
         });
             
-        //check password is matched or not then exec
         if(Emissions){
             let data = {
                 intensity : (Helper.roundToDecimal(Emissions.emission/Emissions.emission_per_ton)).toFixed(1),
@@ -107,7 +107,7 @@ exports.getCustomizeLevers=async(req,res) => {
                     let route = []
                  //   laneData[property.lane_name][property.type] ={};
                     let laneEmissionData = await Emission.findOne({
-                        attributes: ['id', [ sequelize.literal('( SELECT SUM(emission) DIV SUM(total_ton_miles) )'),'intensity'],
+                        attributes: ['id', [ sequelize.literal('( SELECT SUM(AES_DECRYPT(emission,"'+SQLToken+'")) DIV SUM(AES_DECRYPT(total_ton_miles,"'+SQLToken+'")) )'),'intensity'],
                         [ sequelize.literal('( SELECT SUM(shipments) )'),'shipments']],
                         where: {'name':property.lane_name,date: {
                             [Op.between]: [pastData, currentData],
